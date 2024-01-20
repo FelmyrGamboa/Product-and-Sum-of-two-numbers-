36aec61c35e0023203ccf245a000bd497dfc95bd
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-20 18:33:12 +0800

    Psuedocodes

--- product_and_sum_of_two_numbers.py
+++ product_and_sum_of_two_numbers.py
@@ -1,4 +1,12 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
 # Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
+#Psuedocode       
+#Define a function
+#Calculate for the product of the given numbers
+#Calculate for the sum of the given numbers
 
+#Check if the product is equal or less than 1000. If yes, return their product
+#If not return the sum
+
+#Print the results
